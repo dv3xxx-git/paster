617bd012c0a35910f0d780e7952fb254cfba66fd
dv3xxx <dv3xxx@mail.ru>
2021-07-07 11:29:05 +0700

    min fix

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [PasteController::class,'index']);
 Auth::routes();
 Route::group(['middleware' => 'guest'], function () {
     Route::get('/vk/auth', [SocialController::class, 'vk'])->name('vk.auth');
