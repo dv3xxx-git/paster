37e87c496bfeb155c0bdc406d9ba33e05c4e1a8a
dv3xxx-git <dv3xxx@mail.ru>
2021-07-14 23:04:34 +0700

    fix

--- app/Providers/LastPastesProvider.php
+++ app/Providers/LastPastesProvider.php
@@ -31,7 +31,7 @@ class LastPastesProvider extends ServiceProvider
         FacadesView::composer('last_paste', function ($view) {
             $last_paste = Paste::whereAcceptTimer(0)->whereAcceptPublic(0)
                 ->orderByRaw('created_at DESC')
-                ->get();
+                ->take(10)->get();
 
             $view->with([
                 'last_paste' => $last_paste,
@@ -44,8 +44,12 @@ class LastPastesProvider extends ServiceProvider
             if ($auth_user) {
                 $user_pastes = Paste::whereUserId($auth_user->id)
                     ->whereAcceptTimer(0)
-                    ->whereAcceptPublic(0)
-                    ->orWhere('accept_public', 2)->get()
+                    ->where(function($query){
+                    $query->whereAcceptPublic(0)
+                    ->orWhere('accept_public', 2);
+                    })
+                    ->orderByRaw('created_at DESC')
+                    ->get()
                     ->take(10);
 
                 $view->with([
